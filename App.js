c1845c8148daba9a0490050bbbcb146968933572
Emmanuel Ndukwe <emasysnd@gmail.com>
2018-10-12 16:25:32 +0100

    create project boilerplate codes

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import Header from './src/components/common/Header';
+import Albums from './src/components/album/container/Albums';
+
+type Props = {};
+export default class App extends Component<Props> {
+  componentDidMount() {
+    //
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Header title="Hello react native" />
+      </View>
+    );
+  }
+}
---
+++ redux/actions/types.js
@@ -0,0 +1,1 @@
+export const IS_FETCHING = 'is_fetching';
